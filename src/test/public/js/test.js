2a1b8d86d86776a147dc5d76fbebf92e55e82dd0
Tyler <t.nichols89@yahoo.com>
2011-12-03 23:51:48 -0800

    Added some boilerplate code along with an OAuth library.

--- src/test/public/js/test.js
+++ src/test/public/js/test.js
@@ -1,2 +1,41 @@
-/* Hello, friends! Tyler here, figuring stuff out. This is fun, isn't it?! 
- AJAX will be coming soon to a theater near you. */
+/*
+ * To-do:
+ * - Link up with jsOAuth lib
+ * - Get information required for authorization
+ * - Make it functional
+ * 
+ */
+
+var test = function () {
+    var oauth = OAuth({
+    	// I need to acquire this information... and then hide it
+        consumerKey: "<consumer-key>",
+        consumerSecret: "<consumer-secret>"
+    }),
+    
+    success = function (data) {
+    	var timeline = JSON.parse(data.text);
+    	timeline.foreach(function (element) {
+    		alert(element.text);
+    	});
+    },
+    
+    failure = function (data) {
+    	alert("Error")
+    },
+    
+    connect = function () {
+    	// Simple example of retrieving recent Tweets
+    	oauth.get("http://api.twitter.com/1/statuses/home_timeline.json", success, failure);
+    	
+    	// encodeURI()
+    	oauth.post("https://api.twitter.com/oauth/request_token"/*,*/ /* URI encoded callback location */);
+    };
+};
+
+/* Links:
+ * https://dev.twitter.com/docs
+ *     Implementing Sign in with Twitter
+ * 
+ * bytespider.github.com/jsOAuth/api-reference
+ */
