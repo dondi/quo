b1bef3df4bb01b39a3ea137e7caba1d7823794e7
dondi <dondi@lmu.edu>
2012-03-21 02:00:34 -0700

    Some feedback tweaks.

--- src/main/public/js/action/post-status.js
+++ src/main/public/js/action/post-status.js
@@ -1,11 +1,42 @@
 $(function () {
-  $("#post-this").click(function () {
-    $.getJSON("/tweet/" + encodeURIComponent($("#status").val()), function(data) {
-    	if (data.error) {
-    		$("#error-alert").fadeIn("slow");
-    		$("#error-message").text(data.error);
-    	}
-      $("#status").val("").change();
-    });
-  });
+        // Hold a reference to the post-this button because we will use
+        // it a lot.
+    var postThis = $("#post-this"),
+
+        // Name the sendStatus function so that we can use it later.
+        sendStatus = function () {
+            // Grab the current status.
+            var statusText = $("#status").val();
+
+            // Quick and dirty: an empty status is ignored.  What this should
+            // really be is a dynamic disabling of the post-this button when
+            // the text area is empty.
+            if (!statusText) {
+                return;
+            }
+
+            // Put up a little feedback.
+            postThis
+                .addClass("disabled-or-inactive")
+                .unbind("click");
+
+            // Send the status post to the server.
+            $.getJSON("/tweet/" + encodeURIComponent($("#status").val()), function (data) {
+                // If something went wrong, data will have an error property.
+                if (data.error) {
+                    $("#error-alert").fadeIn("slow");
+                    $("#error-message").text(data.error);
+                }
+
+                // No matter what, clear things up for the next status post.
+                $("#status").val("").change();
+
+                // Get the button back in gear.
+                postThis
+                    .click(sendStatus)
+                    .removeClass("disabled-or-inactive");
+            });
+        };
+
+    postThis.click(sendStatus);
 });
