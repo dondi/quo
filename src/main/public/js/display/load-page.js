3e291f8093db1cc6f5b175fb33cfb2f3461514e6
dondi <dondi@lmu.edu>
2012-03-22 12:13:47 -0700

    Tried out some 3D eye candy.

--- src/main/public/js/display/load-page.js
+++ src/main/public/js/display/load-page.js
@@ -1,3 +1,27 @@
 $(function () {
-    $(".wrapper").fadeIn("slow");
+    // We're using .panel a lot, so save it.
+    var panel = $(".panel");
+
+    $(".back-panel")
+        .offset(panel.offset())
+        .width(panel.outerWidth())
+        .height(panel.outerHeight())
+        // Do not allow transitions until size has been set.
+        .css({
+            "transition-property": "all",
+            "transition-duration": "10s"
+        });
+
+    panel.css({
+        "opacity": "1.0",
+        "transform": "rotate3d(0, 1, 0, 0deg)",
+        "-moz-transform": "rotate3d(0, 1, 0, 0deg)",
+        "-webkit-transform": "rotate3d(0, 1, 0, 0deg)"
+    });
+
+    $(".back-panel").css({
+        "transform": "rotate3d(0, 1, 0, 180deg)",
+        "-moz-transform": "rotate3d(0, 1, 0, 180deg)",
+        "-webkit-transform": "rotate3d(0, 1, 0, 180deg)"
+    });
 });
