3717a1dde4d13e726aea3c40e876959fb16beead
dondi <dondi@lmu.edu>
2012-03-22 00:43:36 -0700

    Repositioned the login error message.

--- src/main/public/js/display/login-display.js
+++ src/main/public/js/display/login-display.js
@@ -4,38 +4,39 @@
  * Script for dealing with display elements in the
  * login screen.
  */
-
 $(function () {
-  // Set the focus on the login input
-  $("#user")
-    .focus();
+    // Set the focus on the login input
+    $("#user").focus();
   
-  // Checks given credentials with the DB and either displays an error or redirects
-  $("#login-button").click(function() {
-    var credentials = {
-      user: $("#username").val(),
-      pass: $("#password").val()
-    };
-    
-	  $.ajax({
-      type: 'POST',
-      url: '/',
-      data: credentials,
-      success: function (result) {
-        if (result) {
-          window.location = "./auth/twitter";
-        } else {
-          $("#login-err").fadeIn(1000)
-                         .delay(500)
-                         .fadeOut(1000);
-        }
-      },
-      error: function (jqXHR, textStatus, errorThrown) {
-        console.log(jqXHR);
-        console.log(textStatus);
-        console.log(errorThrown);
-      }
+    // Checks given credentials with the DB and either displays an error or redirects
+    $("#login-button").click(function () {
+        var credentials = {
+            user: $("#username").val(),
+            pass: $("#password").val()
+        };
+
+        $.ajax({
+            type: "POST",
+            url: "/",
+            data: credentials,
+
+            success: function (result) {
+                if (result) {
+                    window.location = "./auth/twitter";
+                } else {
+                    $("#login-err")
+                        .fadeIn(1000)
+                        .delay(2000)
+                        .fadeOut(1000);
+                }
+            },
+
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log(jqXHR);
+                console.log(textStatus);
+                console.log(errorThrown);
+            }
+        });
     });
-  });
-  
+
 });
