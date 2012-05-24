b2234317f21426d6850d3fa4a9626f980934a1d3
dondi <dondi@lmu.edu>
2012-05-24 15:24:20 -0700

    Connected the /profile/:username web page to the just-implemented PUT service for /users/:username.

--- src/main/public/js/action/profile.js
+++ src/main/public/js/action/profile.js
@@ -13,4 +13,23 @@ $(function () {
             $("#user-email").val(result.email);
         }
     );
+
+    // Set up the Save Profile button to perform a PUT to the referenced user.
+    // TODO This page should also be capable of performing a POST in order
+    //      to create new users.
+    $("#save-profile").click(function () {
+        $.ajax(
+            "/users/" + username,
+            {
+                type: "PUT",
+                data: {
+                    email: $("#user-email").val()
+                },
+                success: function (result) {
+                    // TODO This can be much better  :)
+                    alert("Done!");
+                }
+            }
+        );
+    });
 });
