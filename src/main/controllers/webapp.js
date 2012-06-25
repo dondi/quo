778a0cee178718063de577654c1ab82a6c736e02
dondi <dondi@lmu.edu>
2012-06-25 10:54:29 -0700

    First attempt at authentication barrier.

--- src/main/controllers/webapp.js
+++ src/main/controllers/webapp.js
@@ -6,6 +6,13 @@
  */
 module.exports = function (app, database) {
 
+    var loginIntercept = function (req, res) {
+            // For now, just bail with forbidden.
+            // TODO Redirect to a login page instead, and let the login go
+            //      back here.
+            res.send(403);
+        };
+
     /*
      * GET /
      *   Renders the login index
@@ -20,19 +27,49 @@ module.exports = function (app, database) {
      * GET /main
      *   Renders the main screen
      */
-    app.get('/main', function (req, res) {
-        res.render('main', {
-            layout: true
-        });
+    app.get("/main", function (req, res) {
+        if (req.session.user) {
+            res.render("main", {
+                layout: true
+            });
+        } else {
+            loginIntercept(req, res);
+        }
     });
 
     /*
-     *  GET /profile
-     *  renders the profile screen
+     * GET /profile
+     *   Redirects to the profile page of the current user
      */
-    app.get('/profile/:username', function (req, res) {
-	    res.render('profile', {
-            layout: true
+    app.get("/profile", function (req, res) {
+        if (req.session.user) {
+            res.redirect("/profile/" + req.session.user);
+        } else {
+            loginIntercept(req, res);
+        }
+    });
+
+    /*
+     * GET /profile/:username
+     *   Renders the profile page for the given user
+     */
+    app.get("/profile/:username", function (req, res) {
+        if (req.session.user === req.params.username) {
+            res.render('profile', {
+                layout: true
+            });
+        } else {
+            loginIntercept(req, res);
+        }
+    });
+
+    /*
+     * GET /bye
+     *   Logs out the current user then redirects to the login page
+     */
+    app.get("/bye", function (req, res) {
+        req.session.destroy(function () {
+            res.redirect("/");
         });
     });
 
